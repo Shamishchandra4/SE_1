9cfa3314dc4631ac04fe5e4526b859b472447488
Shamishchandra4 <Shamish.chandra@gmail.com>
2024-12-30 09:13:39 +0530

    Support multiple input sets with classification

--- W_I.py
+++ W_I.py
@@ -1,18 +1,18 @@
 with open("input.txt", "r") as file:
-    data = file.readline().strip().split(",")
-    temperature = float(data[0])
-    humidity = float(data[1])
-    windspeed = float(data[2])
-
-weather = 1.5 * temperature - 0.4 * humidity + 0.3 * windspeed - 10
-
-if weather > 4000:
-    classification = "Sunny"
-elif 2000 < weather <= 4000:
-    classification = "Cloudy"
-elif 1000 < weather <= 2000:
-    classification = "Rainy"
-else:
-    classification = "Stormy"
-
-print(f"Input: {data} => Calculated weather: {weather} ({classification})")
+    for line in file:
+        data = line.strip().split(",")
+        temperature = float(data[0])
+        humidity = float(data[1])
+        windspeed = float(data[2])
+        weather = 1.5 * temperature - 0.4 * humidity + 0.3 * windspeed - 10
+        
+        if weather > 4000:
+            classification = "Sunny"
+        elif 2000 < weather <= 4000:
+            classification = "Cloudy"
+        elif 1000 < weather <= 2000:
+            classification = "Rainy"
+        else:
+            classification = "Stormy"
+        
+        print(f"Input: {data} => Calculated weather: {weather} ({classification})")
